73fb5c95fd39b75e65b65563c363fe3d66ad05b3
Gustavo Rezende <gustavorezendesilva@hotmail.com>
2018-10-18 18:55:02 -0300

    🚧 Reading perceptions from /jason/percepts

--- rosbridge_agents/rosbridgeArch.java
+++ rosbridge_agents/rosbridgeArch.java
@@ -8,14 +8,32 @@ import ros.RosBridge;
 import ros.RosListenDelegate;
 import ros.msgs.std_msgs.PrimitiveMsg;
 import ros.tools.MessageUnpacker;
+import ros.SubscriptionRequestMsg;
 
 public class rosbridgeArch extends AgArch {
+
+    RosBridge bridge = new RosBridge();
+
+    @Override
+    public void init(){
+      bridge.connect("ws://master:9090", true);
+      bridge.subscribe(SubscriptionRequestMsg.generate("/jason/percepts")
+                .setType("std_msgs/String")
+                .setThrottleRate(1)
+                .setQueueLength(1),
+        new RosListenDelegate() {
+              public void receive(JsonNode data, String stringRep) {
+                      MessageUnpacker<PrimitiveMsg<String>> unpacker = new MessageUnpacker<PrimitiveMsg<String>>(PrimitiveMsg.class);
+                      PrimitiveMsg<String> msg = unpacker.unpackRosMessage(data);
+                      System.out.println(msg.data);
+              }
+        }
+      );
+    }
 
     @Override
     public List<Literal> perceive() {
 
-      // gets the default perception
-      // List<Literal> per = (List<Literal>) super.perceive();
       List<Literal> per = new ArrayList<Literal>();
       Literal lit = Literal.parseLiteral("p(a)");
 
