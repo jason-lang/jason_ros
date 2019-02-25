b2a3ba00707d7bf7243aa867bfba4b40ccbbc522
Gustavo Rezende <gustavorezendesilva@hotmail.com>
2019-02-25 17:24:01 -0300

    🎨 clang-format

--- rosjava_agents/src/java/RosJasonNode.java
+++ rosjava_agents/src/java/RosJasonNode.java
@@ -14,60 +14,57 @@ import org.ros.node.topic.Publisher;
 import org.ros.node.topic.Subscriber;
 import org.ros.message.MessageListener;
 
-public class RosJasonNode extends AbstractNodeMain{
-	Publisher<std_msgs.String> actionPub;
-	Literal perception = null;
-	List<String> actions_status = new ArrayList<String>();
-	boolean connected;
+public class RosJasonNode extends AbstractNodeMain {
+  Publisher<std_msgs.String> actionPub;
+  Literal perception = null;
+  List<String> actions_status = new ArrayList<String>();
+  boolean connected;
 
-	@Override
-	public GraphName getDefaultNodeName() {
-	  return GraphName.of("jason/agent");
-	}
+  @Override
+  public GraphName getDefaultNodeName() {
+    return GraphName.of("jason/agent");
+  }
 
-	@Override
-    public void onStart(final ConnectedNode connectedNode) {
-		actionPub = connectedNode.newPublisher("/jason/actions", std_msgs.String._TYPE);
+  @Override
+  public void onStart(final ConnectedNode connectedNode) {
+    actionPub =
+        connectedNode.newPublisher("/jason/actions", std_msgs.String._TYPE);
 
-		Subscriber<std_msgs.String> perceptsSub =
-			connectedNode.newSubscriber("/jason/percepts", std_msgs.String._TYPE);
+    Subscriber<std_msgs.String> perceptsSub =
+        connectedNode.newSubscriber("/jason/percepts", std_msgs.String._TYPE);
 
-		perceptsSub.addMessageListener(new MessageListener<std_msgs.String>() {
-	      @Override
-	      public void onNewMessage(std_msgs.String message) {
-	        perception =Literal.parseLiteral(message.getData());
-	      }
-	    });
+    perceptsSub.addMessageListener(new MessageListener<std_msgs.String>() {
+      @Override
+      public void onNewMessage(std_msgs.String message) {
+        perception = Literal.parseLiteral(message.getData());
+      }
+    });
 
-		Subscriber<std_msgs.String> actionsStatusSub =
-		connectedNode.newSubscriber("/jason/action_status", std_msgs.String._TYPE);
+    Subscriber<std_msgs.String> actionsStatusSub = connectedNode.newSubscriber(
+        "/jason/action_status", std_msgs.String._TYPE);
 
-		actionsStatusSub.addMessageListener(new MessageListener<std_msgs.String>() {
-			@Override
-			public void onNewMessage(std_msgs.String message) {
-				actions_status.add(message.getData());
-			}
-		});
-		this.connected= true;
-	}
+    actionsStatusSub.addMessageListener(new MessageListener<std_msgs.String>() {
+      @Override
+      public void onNewMessage(std_msgs.String message) {
+        actions_status.add(message.getData());
+      }
+    });
+    this.connected = true;
+  }
 
-	public Literal getPerception(){
-		return perception;
-	}
+  public Literal getPerception() { return perception; }
 
-	public void publishAction(String action){
-		std_msgs.String str = actionPub.newMessage();
-        str.setData(action);
-		actionPub.publish(str);
-	}
+  public void publishAction(String action) {
+    std_msgs.String str = actionPub.newMessage();
+    str.setData(action);
+    actionPub.publish(str);
+  }
 
-	public List<String> retrieveStatus(){
-		List<String> aux = new ArrayList<String>(actions_status);
-		actions_status.clear();
-		return aux;
-	}
+  public List<String> retrieveStatus() {
+    List<String> aux = new ArrayList<String>(actions_status);
+    actions_status.clear();
+    return aux;
+  }
 
-	public boolean Connected() {
-		return this.connected;
-	}
+  public boolean Connected() { return this.connected; }
 }
--- rosjava_agents/src/java/RosjavaArch.java
+++ rosjava_agents/src/java/RosjavaArch.java
@@ -19,128 +19,140 @@ import org.ros.node.NodeConfiguration;
 import org.ros.node.NodeMain;
 import org.ros.node.NodeMainExecutor;
 
-public class RosjavaArch extends AgArch{
-	RosJasonNode rosNode;
-	Map<String, ActionExec> actionsWaiting = new HashMap<String,ActionExec>();
+public class RosjavaArch extends AgArch {
+  RosJasonNode rosNode;
+  Map<String, ActionExec> actionsWaiting = new HashMap<String, ActionExec>();
 
-	@Override
-	public void init() throws Exception{
-		CommandLineLoader loader = new CommandLineLoader(Lists.newArrayList("RosJasonNode"));
-	    String nodeClassName = loader.getNodeClassName();
-	    System.out.println("Loading node class: " + loader.getNodeClassName());
-	    NodeConfiguration nodeConfiguration = loader.build();
-		NodeMain nodeMain = null;
-	    try {
-	      nodeMain = (RosJasonNode) loader.loadClass(nodeClassName);
-	    } catch (ClassNotFoundException e) {
-	      throw new RosRuntimeException("Unable to locate node: " + nodeClassName, e);
-	    } catch (InstantiationException e) {
-	      throw new RosRuntimeException("Unable to instantiate node: " + nodeClassName, e);
-	    } catch (IllegalAccessException e) {
-	      throw new RosRuntimeException("Unable to instantiate node: " + nodeClassName, e);
-	    }
+  @Override
+  public void init() throws Exception {
+    CommandLineLoader loader =
+        new CommandLineLoader(Lists.newArrayList("RosJasonNode"));
+    String nodeClassName = loader.getNodeClassName();
+    System.out.println("Loading node class: " + loader.getNodeClassName());
+    NodeConfiguration nodeConfiguration = loader.build();
+    NodeMain nodeMain = null;
 
-	    Preconditions.checkState(nodeMain != null);
-	    NodeMainExecutor nodeMainExecutor = DefaultNodeMainExecutor.newDefault();
-	    nodeMainExecutor.execute(nodeMain, nodeConfiguration);
-
-		rosNode = (RosJasonNode) nodeMain;
-		while( !rosNode.Connected() ) Thread.sleep(1000);
-	}
-
-	@Override
-    public void reasoningCycleStarting() {
-    	List<String> actions_status = rosNode.retrieveStatus();
-		for(String status : actions_status){
-			actionsStatus(status);
-		}
-    	super.reasoningCycleStarting();
+    try {
+      nodeMain = (RosJasonNode)loader.loadClass(nodeClassName);
+    } catch (ClassNotFoundException e) {
+      throw new RosRuntimeException("Unable to locate node: " + nodeClassName,
+                                    e);
+    } catch (InstantiationException e) {
+      throw new RosRuntimeException(
+          "Unable to instantiate node: " + nodeClassName, e);
+    } catch (IllegalAccessException e) {
+      throw new RosRuntimeException(
+          "Unable to instantiate node: " + nodeClassName, e);
     }
 
-	@Override
-	public List<Literal> perceive() {
-	  List<Literal> per = new ArrayList<Literal>();
-	  Literal perception = rosNode.getPerception();
-	  if(perception!=null){
-		per.add(perception);
-	  }
-	  return per;
-	}
+    Preconditions.checkState(nodeMain != null);
+    NodeMainExecutor nodeMainExecutor = DefaultNodeMainExecutor.newDefault();
+    nodeMainExecutor.execute(nodeMain, nodeConfiguration);
 
-	@Override
-    public void act(ActionExec action) {
-        String action_string = actionToString(action);
-        rosNode.publishAction(action_string);
+    rosNode = (RosJasonNode)nodeMain;
+    while (!rosNode.Connected())
+      Thread.sleep(1000);
+  }
 
-        actionsWaiting.put(getPredicate(action_string), action); //TODO: Needs to be improved
+  @Override
+  public void reasoningCycleStarting() {
+    List<String> actions_status = rosNode.retrieveStatus();
+    for (String status : actions_status) {
+      actionsStatus(status);
     }
+    super.reasoningCycleStarting();
+  }
 
-	public String actionToString(ActionExec action){
-	 String s = action.getActionTerm().getFunctor();
-	 List<Term> terms = action.getActionTerm().getTerms();
-	   if (action.getActionTerm().hasTerm()){
-		 s = s + "(";
-		 for(Term term : terms){
-		   if(term.isString()){
-			 s = s + ((StringTerm) term).getString() + ",";
-		   } else if(term.isNumeric()){
-			 try{
-			   s = s + Double.toString(((NumberTerm) term).solve()) + ",";
-			 } catch(Exception e) {e.printStackTrace();}
-		   }
-		 }
-		 s = s.substring(0, s.length()-1) + ")"; //take last ',' out and close with ')'
-	   }
-	 return s;
-   }
+  @Override
+  public List<Literal> perceive() {
+    List<Literal> per = new ArrayList<Literal>();
+    Literal perception = rosNode.getPerception();
+    if (perception != null) {
+      per.add(perception);
+    }
+    return per;
+  }
 
-   public void actionsStatus(String data){
-	 String action_status = getPredicate(data);
-	 String action_name = getArgs(data);
+  @Override
+  public void act(ActionExec action) {
+    String action_string = actionToString(action);
+    rosNode.publishAction(action_string);
 
-	 if (action_status!=null && action_name!=null) {
-	   // System.out.println("Action: " + action_name + " Status: " + action_status);
-	   ActionExec action = actionsWaiting.get(action_name);
+    actionsWaiting.put(getPredicate(action_string),
+                       action); // TODO: Needs to be improved
+  }
 
-	   if(action != null){
-		 if(action_status.equals("done")){
-		   action.setResult(true);
-		   actionExecuted(action);
-		 }else if(action_status.equals("fail")){
-		   action.setResult(false);
-		   actionExecuted(action);
-		 }else{
-		   System.out.println("Status" + action_status + "unknown. Setting action "+action_name + " as failed!");
-		   action.setResult(false);
-		   actionExecuted(action);
-		 }
-		 actionsWaiting.remove(action_name);
-	   }else{
-		 System.out.println("Action "+action_name+" not found.");
-	   }
-	 }else {
-	   System.out.println("Couldn't confirm action status");
-	 }
+  public String actionToString(ActionExec action) {
+    String s = action.getActionTerm().getFunctor();
+    List<Term> terms = action.getActionTerm().getTerms();
+    if (action.getActionTerm().hasTerm()) {
+      s = s + "(";
+      for (Term term : terms) {
+        if (term.isString()) {
+          s = s + ((StringTerm)term).getString() + ",";
+        } else if (term.isNumeric()) {
+          try {
+            s = s + Double.toString(((NumberTerm)term).solve()) + ",";
+          } catch (Exception e) {
+            e.printStackTrace();
+          }
+        }
+      }
+      s = s.substring(0, s.length() - 1) +
+          ")"; // take last ',' out and close with ')'
+    }
+    return s;
+  }
 
-   }
+  public void actionsStatus(String data) {
+    String action_status = getPredicate(data);
+    String action_name = getArgs(data);
 
-   public String getPredicate(String data){
-	 Pattern p = Pattern.compile("[^(]*");
-	 Matcher m = p.matcher(data);
-	 String predicate = null;
-	 if(m.find()){
-	   predicate = m.group(0);
-	 }
-	 return predicate;
-   }
+    if (action_status != null && action_name != null) {
+      // System.out.println("Action: " + action_name + " Status: " +
+      // action_status);
+      ActionExec action = actionsWaiting.get(action_name);
 
-   public String getArgs(String data){
-	 Pattern p = Pattern.compile("\\((.*?)\\)");
-	 Matcher m = p.matcher(data);
-	 String args = null;
-	 if(m.find()){
-	   args = m.group(1);
-	 }
-	 return args;
-   }
+      if (action != null) {
+        if (action_status.equals("done")) {
+          action.setResult(true);
+          actionExecuted(action);
+        } else if (action_status.equals("fail")) {
+          action.setResult(false);
+          actionExecuted(action);
+        } else {
+          System.out.println("Status" + action_status +
+                             "unknown. Setting action " + action_name +
+                             " as failed!");
+          action.setResult(false);
+          actionExecuted(action);
+        }
+        actionsWaiting.remove(action_name);
+      } else {
+        System.out.println("Action " + action_name + " not found.");
+      }
+    } else {
+      System.out.println("Couldn't confirm action status");
+    }
+  }
+
+  public String getPredicate(String data) {
+    Pattern p = Pattern.compile("[^(]*");
+    Matcher m = p.matcher(data);
+    String predicate = null;
+    if (m.find()) {
+      predicate = m.group(0);
+    }
+    return predicate;
+  }
+
+  public String getArgs(String data) {
+    Pattern p = Pattern.compile("\\((.*?)\\)");
+    Matcher m = p.matcher(data);
+    String args = null;
+    if (m.find()) {
+      args = m.group(1);
+    }
+    return args;
+  }
 }
