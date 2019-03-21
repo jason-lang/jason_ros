a03558b0d52c7b570e6d48a86e75e3a00d2030c1
Gustavo Rezende <gustavorezendesilva@hotmail.com>
2019-03-20 21:35:59 -0300

    🚧 ComController as super class of ActionController

--- HwBridge/hw_controller.py
+++ HwBridge/hw_controller.py
@@ -4,7 +4,7 @@ import importlib
 from pathlib import Path
 import rospy
 
-class ActionInfo:
+class CommInfo:
     def __init__(self):
         self.name = ""
         self.comm_method = ""
@@ -13,43 +13,53 @@ class ActionInfo:
         self.dependencies = ""
         self.module = None
 
-    def fill_data(self, action_name, reader):
-        self.name = action_name
-        if reader.has_option(action_name, "comm_method"):
-            self.comm_method = reader.get(action_name, "comm_method")
-        if reader.has_option(action_name, "comm_msg"):
-            self.comm_msg = reader.get(action_name, "comm_msg")
-        if reader.has_option(action_name, "comm_name"):
-            self.comm_name = reader.get(action_name, "comm_name")
-        if reader.has_option(action_name, "dependencies"):
-            self.dependencies = reader.get(action_name, "dependencies")
+    def fill_data(self, comm_name, reader):
+        self.name = comm_name
+        if reader.has_option(comm_name, "comm_method"):
+            self.comm_method = reader.get(comm_name, "comm_method")
+        if reader.has_option(comm_name, "comm_msg"):
+            self.comm_msg = reader.get(comm_name, "comm_msg")
+        if reader.has_option(comm_name, "comm_name"):
+            self.comm_name = reader.get(comm_name, "comm_name")
+        if reader.has_option(comm_name, "dependencies"):
+            self.dependencies = reader.get(comm_name, "dependencies")
             self.module = importlib.import_module(self.dependencies)
 
-
-class ActionController:
+class CommController:
     def __init__(self):
-        self.action_dict = dict()
+        self.comm_dict = dict()
+        self.default_path = ""
 
-    def read_manifest(self, filename="actions_manifest"):
+    def read_manifest(self, *args):
         reader = ConfigParser.RawConfigParser()
-        man_path = Path(filename)
+        if len(args) == 0:
+            man_path = Path(self.default_path)
+        else:
+            man_path = Path(args[0])
+
         if man_path.is_file():
             reader.read(str(man_path))
             self.get_info(reader)
 
 
     def get_info(self, reader):
-        action_list = reader.sections()
-        for action in action_list:
-            action_info = ActionInfo()
-            action_info.fill_data(action, reader)
-            self.action_dict[action] = action_info
+        comm_list = reader.sections()
+        for comm in comm_list:
+            comm_info = CommInfo()
+            comm_info.fill_data(comm, reader)
+            self.comm_dict[comm] = comm_info
+
+
+class ActionController(CommController):
+    def __init__(self):
+        CommController.__init__(self)
+        self.default_path = "actions_manifest"
 
     def executable_action(self, action_name):
-        return (action_name in self.action_dict.keys())
+        return (action_name in self.comm_dict.keys())
 
     def perform_action(self, action_name, **kw):
-        action = self.action_dict[action_name]
+        action = self.comm_dict[action_name]
         if action.comm_method == "service":
             rospy.wait_for_service(action.comm_name)
             try:
