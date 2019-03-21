dbea97040e388ca06d779f8666e076eb3fc2b9b5
Gustavo Rezende <gustavorezendesilva@hotmail.com>
2019-03-20 21:37:56 -0300

    🚧 Perceving what is defined in perceptions_manifest

--- HwBridge/hw_bridge.py
+++ HwBridge/hw_bridge.py
@@ -14,6 +14,12 @@ def main():
     if a.executable_action('set_mode'):
         a.perform_action('set_mode', custom_mode='GUIDED')
 
+    p = PerceptionController()
+    p.read_manifest()
+    p.start_perceiving()
+    while not rospy.is_shutdown():
+        print(p.perceptions)
+        rate.sleep()
     rospy.spin()
 
 if __name__ == '__main__':
