0f5d62be9acbf36189d9ed4dd688030f58718f8c
Gustavo Rezende <gustavorezendesilva@hotmail.com>
2019-07-29 10:17:40 -0300

    🎨 Formatting code

--- rosjava_agents/src/java/jasonros/RosArch.java
+++ rosjava_agents/src/java/jasonros/RosArch.java
@@ -1,19 +1,6 @@
 package jasonros;
 
-import jason.asSyntax.*;
-import jason.architecture.*;
-import jason.asSemantics.*;
 import static jason.asSyntax.ASSyntax.*;
-import jason.asSyntax.parser.ParseException;
-import jason.RevisionFailedException;
-import jason.asSyntax.Trigger.TEOperator;
-import jason.asSyntax.Trigger.TEType;
-
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.HashMap;
-import java.util.Iterator;
 
 import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
@@ -25,157 +12,167 @@ import org.ros.node.NodeConfiguration;
 import org.ros.node.NodeMain;
 import org.ros.node.NodeMainExecutor;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import jason.RevisionFailedException;
+import jason.architecture.*;
+import jason.asSemantics.*;
+import jason.asSyntax.*;
+import jason.asSyntax.Trigger.TEOperator;
+import jason.asSyntax.Trigger.TEType;
+import jason.asSyntax.parser.ParseException;
 import jason_msgs.ActionStatus;
-import jason_msgs.Perception;
 import jason_msgs.Message;
+import jason_msgs.Perception;
 
 public class RosArch extends AgArch {
-  jasonros.RosJasonNode rosNode;
-  Map<Integer, ActionExec> actionsWaiting = new HashMap<Integer, ActionExec>();
+    jasonros.RosJasonNode rosNode;
+    Map<Integer, ActionExec> actionsWaiting = new HashMap<Integer, ActionExec>();
 
-  @Override
-  public void init() throws Exception {
-    CommandLineLoader loader =
-        new CommandLineLoader(Lists.newArrayList("jasonros.RosJasonNode"));
-    String nodeClassName = loader.getNodeClassName();
-    System.out.println("Loading node class: " + loader.getNodeClassName());
-    NodeConfiguration nodeConfiguration = loader.build();
-    NodeMain nodeMain = null;
+    @Override
+    public void init() throws Exception {
+        CommandLineLoader loader =
+                new CommandLineLoader(Lists.newArrayList("jasonros.RosJasonNode"));
+        String nodeClassName = loader.getNodeClassName();
+        System.out.println("Loading node class: " + loader.getNodeClassName());
+        NodeConfiguration nodeConfiguration = loader.build();
+        NodeMain nodeMain = null;
 
-    try {
-      nodeMain = (jasonros.RosJasonNode)loader.loadClass(nodeClassName);
-    } catch (ClassNotFoundException e) {
-      throw new RosRuntimeException("Unable to locate node: " + nodeClassName,
-                                    e);
-    } catch (InstantiationException e) {
-      throw new RosRuntimeException(
-          "Unable to instantiate node: " + nodeClassName, e);
-    } catch (IllegalAccessException e) {
-      throw new RosRuntimeException(
-          "Unable to instantiate node: " + nodeClassName, e);
-    }
-
-    Preconditions.checkState(nodeMain != null);
-    NodeMainExecutor nodeMainExecutor = DefaultNodeMainExecutor.newDefault();
-    nodeMainExecutor.execute(nodeMain, nodeConfiguration);
-
-    rosNode = (jasonros.RosJasonNode)nodeMain;
-    while (!rosNode.Connected())
-      Thread.sleep(1000);
-  }
-
-  @Override
-  public void reasoningCycleStarting() {
-    List<jason_msgs.ActionStatus> actions_status = rosNode.retrieveStatus();
-    for (jason_msgs.ActionStatus status : actions_status) {
-      actionsStatus(status);
-    }
-    super.reasoningCycleStarting();
-  }
-
-  @Override
-  public List<Literal> perceive(){
-    jason_msgs.Perception perception = rosNode.getPerception();
-    while (perception!=null) {
-        TransitionSystem ts = getTS();
-        Literal bel = createLiteral(perception.getPerceptionName());
-        bel.addAnnot(ts.getAg().getBB().TPercept);
-
-        for(String param: perception.getParameters()){
-            try{
-                Term p = parseTerm(param);
-                if(p.isVar()){
-                    p = new StringTermImpl(param);
-                }
-                bel.addTerm(p);
-            }catch(ParseException e){
-                bel.addTerm(new StringTermImpl(param));
-            }
+        try {
+            nodeMain = (jasonros.RosJasonNode) loader.loadClass(nodeClassName);
+        } catch (ClassNotFoundException e) {
+            throw new RosRuntimeException("Unable to locate node: " + nodeClassName, e);
+        } catch (InstantiationException e) {
+            throw new RosRuntimeException("Unable to instantiate node: " + nodeClassName, e);
+        } catch (IllegalAccessException e) {
+            throw new RosRuntimeException("Unable to instantiate node: " + nodeClassName, e);
         }
 
-        boolean bufUpdate = perception.getUpdate();
-        if(bufUpdate){
-            Iterator<Literal> ibb = ts.getAg().getBB().getCandidateBeliefs(new PredicateIndicator(perception.getPerceptionName(),perception.getParameters().size()));
-            boolean addBelief = true;
-            while (ibb != null && ibb.hasNext()) {
-                Literal l = ibb.next();
-                if(l.equals(bel)){
-                    addBelief = false;
-                }else{
-                   ibb.remove(); // remove l as perception from BB
+        Preconditions.checkState(nodeMain != null);
+        NodeMainExecutor nodeMainExecutor = DefaultNodeMainExecutor.newDefault();
+        nodeMainExecutor.execute(nodeMain, nodeConfiguration);
 
-                   // only produce -bel event if the agent has plans for the event
-                   Trigger te = new Trigger(TEOperator.del, TEType.belief, l);
-                   if (ts.getC().hasListener() || ts.getAg().getPL().hasCandidatePlan(te)) {
-                       l = ASSyntax.createLiteral(l.getFunctor(), l.getTermsArray());
-                       l.addAnnot(ts.getAg().getBB().TPercept);
-                       te.setLiteral(l);
-                       ts.getC().addEvent(new Event(te, Intention.EmptyInt));
-                   }
+        rosNode = (jasonros.RosJasonNode) nodeMain;
+        while (!rosNode.Connected()) Thread.sleep(1000);
+    }
 
-               }
+    @Override
+    public void reasoningCycleStarting() {
+        List<jason_msgs.ActionStatus> actions_status = rosNode.retrieveStatus();
+        for (jason_msgs.ActionStatus status : actions_status) {
+            actionsStatus(status);
+        }
+        super.reasoningCycleStarting();
+    }
+
+    @Override
+    public List<Literal> perceive() {
+        jason_msgs.Perception perception = rosNode.getPerception();
+        while (perception != null) {
+            TransitionSystem ts = getTS();
+            Literal bel = createLiteral(perception.getPerceptionName());
+            bel.addAnnot(ts.getAg().getBB().TPercept);
+
+            for (String param : perception.getParameters()) {
+                try {
+                    Term p = parseTerm(param);
+                    if (p.isVar()) {
+                        p = new StringTermImpl(param);
+                    }
+                    bel.addTerm(p);
+                } catch (ParseException e) {
+                    bel.addTerm(new StringTermImpl(param));
+                }
             }
-            if(addBelief){
-                try{
-                    ts.getAg().addBel(bel);
-                }catch(RevisionFailedException e){
+
+            boolean bufUpdate = perception.getUpdate();
+            if (bufUpdate) {
+                Iterator<Literal> ibb =
+                        ts.getAg().getBB().getCandidateBeliefs(new PredicateIndicator(
+                                perception.getPerceptionName(), perception.getParameters().size()));
+                boolean addBelief = true;
+                while (ibb != null && ibb.hasNext()) {
+                    Literal l = ibb.next();
+                    if (l.equals(bel)) {
+                        addBelief = false;
+                    } else {
+                        ibb.remove(); // remove l as perception from BB
+
+                        // only produce -bel event if the agent has plans for the event
+                        Trigger te = new Trigger(TEOperator.del, TEType.belief, l);
+                        if (ts.getC().hasListener() || ts.getAg().getPL().hasCandidatePlan(te)) {
+                            l = ASSyntax.createLiteral(l.getFunctor(), l.getTermsArray());
+                            l.addAnnot(ts.getAg().getBB().TPercept);
+                            te.setLiteral(l);
+                            ts.getC().addEvent(new Event(te, Intention.EmptyInt));
+                        }
+                    }
+                }
+                if (addBelief) {
+                    try {
+                        ts.getAg().addBel(bel);
+                    } catch (RevisionFailedException e) {
+                        System.out.println("Error adding new belief");
+                    }
+                }
+            } else {
+                try {
+                    getTS().getAg().addBel(bel);
+                } catch (RevisionFailedException e) {
                     System.out.println("Error adding new belief");
                 }
             }
-        }else{
-            try{
-                getTS().getAg().addBel(bel);
-            }catch(RevisionFailedException e){
-                System.out.println("Error adding new belief");
-            }
+            perception = rosNode.getPerception();
         }
-        perception = rosNode.getPerception();
+        return null;
     }
-    return null;
-  }
 
-  @Override
-  public void act(ActionExec action) {
-    int seq = rosNode.publishAction(action);
+    @Override
+    public void act(ActionExec action) {
+        int seq = rosNode.publishAction(action);
 
-    actionsWaiting.put(seq, action);
-  }
-
-  public void actionsStatus(jason_msgs.ActionStatus data) {
-    int id = data.getId();
-    boolean result = data.getResult();
-
-    ActionExec action = actionsWaiting.get(id);
-
-    if (action != null) {
-        action.setResult(result);
-        actionExecuted(action);
-        actionsWaiting.remove(id);
-    } else {
-        System.out.println("Action not found.");
+        actionsWaiting.put(seq, action);
     }
-  }
 
-  @Override
-   public void sendMsg(jason.asSemantics.Message m){
-       rosNode.publishMessage(m);
-   }
+    public void actionsStatus(jason_msgs.ActionStatus data) {
+        int id = data.getId();
+        boolean result = data.getResult();
 
-   @Override
-   public void checkMail(){
-       jason_msgs.Message rosmsg = rosNode.getMessage();
-       Circumstance C = getTS().getC();
-       while (rosmsg!=null) {
-           jason.asSemantics.Message im = new jason.asSemantics.Message();
+        ActionExec action = actionsWaiting.get(id);
 
-           im.setReceiver(rosmsg.getReceiver());
-           im.setSender(rosmsg.getSender());
-           im.setPropCont(createLiteral(rosmsg.getData()));
-           im.setIlForce(rosmsg.getItlforce());
+        if (action != null) {
+            action.setResult(result);
+            actionExecuted(action);
+            actionsWaiting.remove(id);
+        } else {
+            System.out.println("Action not found.");
+        }
+    }
 
-           C.addMsg(im);
+    @Override
+    public void sendMsg(jason.asSemantics.Message m) {
+        rosNode.publishMessage(m);
+    }
 
-           rosmsg = rosNode.getMessage();
-       }
-   }
+    @Override
+    public void checkMail() {
+        jason_msgs.Message rosmsg = rosNode.getMessage();
+        Circumstance C = getTS().getC();
+        while (rosmsg != null) {
+            jason.asSemantics.Message im = new jason.asSemantics.Message();
+
+            im.setReceiver(rosmsg.getReceiver());
+            im.setSender(rosmsg.getSender());
+            im.setPropCont(createLiteral(rosmsg.getData()));
+            im.setIlForce(rosmsg.getItlforce());
+
+            C.addMsg(im);
+
+            rosmsg = rosNode.getMessage();
+        }
+    }
 }
--- rosjava_agents/src/java/jasonros/RosJasonNode.java
+++ rosjava_agents/src/java/jasonros/RosJasonNode.java
@@ -1,35 +1,37 @@
 package jasonros;
 
-import jason.asSyntax.*;
-import jason.architecture.*;
-import jason.asSemantics.*;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.concurrent.ConcurrentLinkedQueue;
-
 import org.apache.commons.logging.Log;
 import org.ros.concurrent.CancellableLoop;
+import org.ros.message.MessageListener;
 import org.ros.namespace.GraphName;
 import org.ros.node.AbstractNodeMain;
-import org.ros.node.NodeConfiguration;
 import org.ros.node.ConnectedNode;
+import org.ros.node.NodeConfiguration;
 import org.ros.node.NodeMain;
 import org.ros.node.topic.Publisher;
 import org.ros.node.topic.Subscriber;
-import org.ros.message.MessageListener;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+import jason.architecture.*;
+import jason.asSemantics.*;
+import jason.asSyntax.*;
 import jason_msgs.Action;
 import jason_msgs.ActionStatus;
-import jason_msgs.Perception;
 import jason_msgs.Message;
+import jason_msgs.Perception;
 
 public class RosJasonNode extends AbstractNodeMain {
     NodeConfiguration nodeConfiguration = NodeConfiguration.newPrivate();
     Publisher<jason_msgs.Action> actionPub;
     Publisher<jason_msgs.Message> msgPub;
 
-    ConcurrentLinkedQueue<jason_msgs.Perception> perceptionQueue = new ConcurrentLinkedQueue<jason_msgs.Perception>();
-    ConcurrentLinkedQueue<jason_msgs.Message> messageQueue = new ConcurrentLinkedQueue<jason_msgs.Message>();
+    ConcurrentLinkedQueue<jason_msgs.Perception> perceptionQueue =
+            new ConcurrentLinkedQueue<jason_msgs.Perception>();
+    ConcurrentLinkedQueue<jason_msgs.Message> messageQueue =
+            new ConcurrentLinkedQueue<jason_msgs.Message>();
 
     List<jason_msgs.ActionStatus> actions_status = new ArrayList<jason_msgs.ActionStatus>();
     boolean connected;
@@ -39,93 +41,99 @@ public class RosJasonNode extends AbstractNodeMain {
         return GraphName.of("jason/agent");
     }
 
-  @Override
-  public void onStart(final ConnectedNode connectedNode) {
-    actionPub =
-        connectedNode.newPublisher("/jason/actions", jason_msgs.Action._TYPE);
+    @Override
+    public void onStart(final ConnectedNode connectedNode) {
+        actionPub = connectedNode.newPublisher("/jason/actions", jason_msgs.Action._TYPE);
 
-    msgPub = connectedNode.newPublisher("/jason/send_msg", jason_msgs.Message._TYPE);
+        msgPub = connectedNode.newPublisher("/jason/send_msg", jason_msgs.Message._TYPE);
 
-    Subscriber<jason_msgs.Perception> perceptsSub =
-        connectedNode.newSubscriber("/jason/percepts", jason_msgs.Perception._TYPE);
+        Subscriber<jason_msgs.Perception> perceptsSub =
+                connectedNode.newSubscriber("/jason/percepts", jason_msgs.Perception._TYPE);
 
-    perceptsSub.addMessageListener(new MessageListener<jason_msgs.Perception>() {
-      @Override
-      public void onNewMessage(jason_msgs.Perception message) {
-        perceptionQueue.offer(message);
-      }
-    });
-
-	Subscriber<jason_msgs.ActionStatus> actionsStatusSub =
-	connectedNode.newSubscriber("/jason/actions_status", jason_msgs.ActionStatus._TYPE);
-
-    actionsStatusSub.addMessageListener(new MessageListener<jason_msgs.ActionStatus>() {
-      @Override
-      public void onNewMessage(jason_msgs.ActionStatus message) {
-        actions_status.add(message);
-      }
-    });
-
-	Subscriber<jason_msgs.Message> msgSub =
-	connectedNode.newSubscriber("/jason/receive_msg", jason_msgs.Message._TYPE);
-
-    msgSub.addMessageListener(new MessageListener<jason_msgs.Message>() {
-      @Override
-      public void onNewMessage(jason_msgs.Message message) {
-          messageQueue.offer(message);
-      }
-    });
-
-    this.connected = true;
-  }
-
-  public jason_msgs.Perception getPerception() { return perceptionQueue.poll(); }
-  public jason_msgs.Message getMessage() { return messageQueue.poll(); }
-
-  public int publishAction(ActionExec action) {
-    jason_msgs.Action act = actionPub.newMessage();
-
-    std_msgs.Header header = nodeConfiguration.getTopicMessageFactory().newFromType(std_msgs.Header._TYPE);
-    act.setHeader(header);
-
-    act.setActionName(action.getActionTerm().getFunctor());
-
-    if (action.getActionTerm().hasTerm()) {
-        List<String> params = new ArrayList<String>();
-        for (Term term : action.getActionTerm().getTerms()) {
-            if(term.isString()){
-                params.add(((StringTerm)term).getString());
-            }else{
-                params.add(String.valueOf(term));
+        perceptsSub.addMessageListener(new MessageListener<jason_msgs.Perception>() {
+            @Override
+            public void onNewMessage(jason_msgs.Perception message) {
+                perceptionQueue.offer(message);
             }
-        }
-        act.setParameters(params);
+        });
+
+        Subscriber<jason_msgs.ActionStatus> actionsStatusSub =
+                connectedNode.newSubscriber("/jason/actions_status", jason_msgs.ActionStatus._TYPE);
+
+        actionsStatusSub.addMessageListener(new MessageListener<jason_msgs.ActionStatus>() {
+            @Override
+            public void onNewMessage(jason_msgs.ActionStatus message) {
+                actions_status.add(message);
+            }
+        });
+
+        Subscriber<jason_msgs.Message> msgSub =
+                connectedNode.newSubscriber("/jason/receive_msg", jason_msgs.Message._TYPE);
+
+        msgSub.addMessageListener(new MessageListener<jason_msgs.Message>() {
+            @Override
+            public void onNewMessage(jason_msgs.Message message) {
+                messageQueue.offer(message);
+            }
+        });
+
+        this.connected = true;
     }
 
-    actionPub.publish(act);
-    return header.getSeq();
-  }
+    public jason_msgs.Perception getPerception() {
+        return perceptionQueue.poll();
+    }
+    public jason_msgs.Message getMessage() {
+        return messageQueue.poll();
+    }
 
-  public void publishMessage(jason.asSemantics.Message m){
-      jason_msgs.Message msg = msgPub.newMessage();
+    public int publishAction(ActionExec action) {
+        jason_msgs.Action act = actionPub.newMessage();
 
-      std_msgs.Header header = nodeConfiguration.getTopicMessageFactory().newFromType(std_msgs.Header._TYPE);
-      msg.setHeader(header);
+        std_msgs.Header header =
+                nodeConfiguration.getTopicMessageFactory().newFromType(std_msgs.Header._TYPE);
+        act.setHeader(header);
 
-      msg.setReceiver(m.getReceiver());
-      msg.setSender(m.getSender());
+        act.setActionName(action.getActionTerm().getFunctor());
 
-      msg.setData(m.getPropCont().toString());
-      msg.setItlforce(m.getIlForce());
-      msgPub.publish(msg);
-  }
+        if (action.getActionTerm().hasTerm()) {
+            List<String> params = new ArrayList<String>();
+            for (Term term : action.getActionTerm().getTerms()) {
+                if (term.isString()) {
+                    params.add(((StringTerm) term).getString());
+                } else {
+                    params.add(String.valueOf(term));
+                }
+            }
+            act.setParameters(params);
+        }
 
-  public List<jason_msgs.ActionStatus> retrieveStatus() {
-    List<jason_msgs.ActionStatus> aux = new ArrayList<jason_msgs.ActionStatus>(actions_status);
-    actions_status.clear();
-    return aux;
-  }
+        actionPub.publish(act);
+        return header.getSeq();
+    }
 
-  public boolean Connected() { return this.connected; }
+    public void publishMessage(jason.asSemantics.Message m) {
+        jason_msgs.Message msg = msgPub.newMessage();
 
+        std_msgs.Header header =
+                nodeConfiguration.getTopicMessageFactory().newFromType(std_msgs.Header._TYPE);
+        msg.setHeader(header);
+
+        msg.setReceiver(m.getReceiver());
+        msg.setSender(m.getSender());
+
+        msg.setData(m.getPropCont().toString());
+        msg.setItlforce(m.getIlForce());
+        msgPub.publish(msg);
+    }
+
+    public List<jason_msgs.ActionStatus> retrieveStatus() {
+        List<jason_msgs.ActionStatus> aux = new ArrayList<jason_msgs.ActionStatus>(actions_status);
+        actions_status.clear();
+        return aux;
+    }
+
+    public boolean Connected() {
+        return this.connected;
+    }
 }
